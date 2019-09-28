550725c92e48edf5e230109445223d6a9b094130
Alexis Aguilera <alexisaguilera@Alexiss-MacBook-Pro.local>
2019-09-28 12:14:02 -0300

    final

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,42 @@
-const gulp = require('gulp');
-const browserSync = require('browser-sync').create();
-const sass = require('gulp-sass');
-const autoprefixer = require('gulp-autoprefixer');
+const gulp = require("gulp");
+const browserSync = require("browser-sync").create();
+const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
 
-gulp.task('sass', () => {
-  return gulp.src([
-    'node_modules/slick-carousel/slick/slick.scss',
-    'src/scss/*.scss'
-  ])
-  .pipe(sass({outputStyle: 'expanded'}))
-  .pipe(autoprefixer({
-    browsers: ['last 2 versions'],
-    cascade: false
-    }))
-  .pipe(gulp.dest('src/css'))
-  .pipe(browserSync.stream());
+gulp.task("sass", () => {
+  return gulp
+    .src(["node_modules/slick-carousel/slick/slick.scss", "src/scss/*.scss"])
+    .pipe(sass({ outputStyle: "expanded" }))
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 versions"],
+        cascade: false
+      })
+    )
+    .pipe(gulp.dest("src/css"))
+    .pipe(browserSync.stream());
 });
 
-gulp.task('js', () => {
-  return gulp.src([
-    'node_modules/jquery/dist/jquery.min.js',
-    'node_modules/popper.js/dist/umd/popper.min.js',
-    'node_modules/slick-carousel/slick/slick.js',
-  ])
-  .pipe(gulp.dest('src/js'))
-  .pipe(browserSync.stream());
+gulp.task("js", () => {
+  return gulp
+    .src([
+      "node_modules/jquery/dist/jquery.min.js",
+      "node_modules/slick-carousel/slick/slick.js"
+    ])
+    .pipe(gulp.dest("src/js"))
+    .pipe(browserSync.stream());
 });
 
-gulp.task('serve', ['sass'], () => {
+gulp.task("serve", ["sass"], () => {
   browserSync.init({
-    server: './src'
+    server: "./src"
   });
 
-  gulp.watch([
-    'node_modules/slick-carousel/slick/slick.scss',
-    'src/scss/*.scss'
-  ], ['sass']);
-  gulp.watch('src/*.html').on('change', browserSync.reload);
+  gulp.watch(
+    ["node_modules/slick-carousel/slick/slick.scss", "src/scss/*.scss"],
+    ["sass"]
+  );
+  gulp.watch("src/*.html").on("change", browserSync.reload);
 });
 
-
-gulp.task('default', ['js', 'serve'])
+gulp.task("default", ["js", "serve"]);
